f744ea13f8428e4f98609909318999364f03a11c
ryo7400s-del <ryo7400.s@gmail.com>
2026-05-12 16:18:16 +0900

    remove coinbase wallet connector

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,14 +3,13 @@ import { OnchainKitProvider } from "@coinbase/onchainkit";
 import { base } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider, createConfig, http } from "wagmi";
-import { coinbaseWallet, metaMask, injected } from "wagmi/connectors";
+import { metaMask, injected, walletConnect } from "wagmi/connectors";
 
 const wagmiConfig = createConfig({
   chains: [base],
   connectors: [
-    coinbaseWallet({ appName: "Rock Scissors Paper" }),
-    metaMask(),
     injected(),
+    metaMask(),
   ],
   transports: {
     [base.id]: http(),
