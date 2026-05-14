15f9b533aec162663879cb54447cff1a7a235e22
ryo7400s-del <ryo7400.s@gmail.com>
2026-05-14 17:37:06 +0900

    fix web3modal client side init

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,8 @@ import { OnchainKitProvider } from "@coinbase/onchainkit";
 import { base } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
-import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi";
+import { defaultWagmiConfig } from "@web3modal/wagmi";
+import { createWeb3Modal } from "@web3modal/wagmi/react";
 
 const projectId = "60ed604513c97a143e29ba2c149d396d";
 
@@ -24,6 +25,7 @@ createWeb3Modal({
   wagmiConfig,
   projectId,
   defaultChain: base,
+  allowUnsupportedChain: false,
 });
 
 const queryClient = new QueryClient();
